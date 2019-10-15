5ae9126b2bb71f8deb9d58120f8a79e66e1a30d7
Philip Lutz <philiplutz413@gmail.com>
2019-10-14 17:16:20 -0700

    added queries for users and grades

--- db/queries.js
+++ db/queries.js
@@ -1,2 +1,70 @@
 const knex = require('./knex.js');
 
+// Users
+function Users() {
+	return knex('users');
+}
+
+function getAllUsers() {
+	return Users().select();
+}
+
+function getSingleUser(userId) {
+  return Users().where('user_id', userId).first();
+}
+
+function addUser(user) {
+	return Users().insert(user, 'user_id');
+}
+
+function updateUser(userId, updates) {
+	return Users().where('user_id', userId).update(updates);
+}
+
+function deleteUser(userId) {
+	return Users().where('user_id', userId).del();
+}
+
+// Grades
+function Grades() {
+	return knex('grades');
+}
+
+function getAllGrades() {
+	return Grades().select();
+}
+
+function getAllUserGrades(userId) {
+	return Grades().where('user_id', userId);
+}
+
+function getSingleGrade(gradeId) {
+	return Grades().where('grade_id', gradeId).first();
+}
+
+function addGrade(grade) {
+	return Grades().insert(grade, 'grade_id');
+}
+
+function updateGrade(gradeId, updates) {
+	return Grades().where('grade_id', gradeId).update(updates);
+}
+
+function deleteGrade(gradeId) {
+	return Grades().where('grade_id', gradeId).del();
+}
+
+
+module.exports = {
+	getAllUsers: getAllUsers,
+	getSingleUser: getSingleUser,
+	addUser: addUser,
+	updateUser: updateUser,
+	deleteUser: deleteUser,
+	getAllGrades: getAllGrades,
+	getAllUserGrades: getAllUserGrades,
+	getSingleGrade: getSingleGrade,
+	addGrade: addGrade,
+	updateGrade: updateGrade,
+	deleteGrade: deleteGrade
+};
