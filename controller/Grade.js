1262318f77cc8d94a6e4c801e2790697e7e1bccc
Philip Lutz <philiplutz413@gmail.com>
2019-10-25 17:08:27 -0700

    added grade routes

--- controller/Grade.js
+++ controller/Grade.js
@@ -5,19 +5,104 @@ const queries = require('../db/queries.js');
 const Auth = require('../auth/authentication.js');
 
 const Grade = {
-	getSingleGrade(req, res, next) {
-
-	},
 	getAllGrades(req, res, next) {
-
+		queries.getAllGrades()
+		.then(function(grades) {
+			return res.status(200).send(grades);
+		})
+		.catch(function(error) {
+			next(error);
+		})
+	},
+	getAllUserGrades(req, res, next) {
+		queries.getAllUserGrades(req.params.user_id)
+		.then(function(grades) {
+			return res.status(200).send(grades);
+		})
+		.catch(function(error) {
+			next(error);
+		})
+	},
+	getSingleGrade(req, res, next) {
+		queries.getSingleGrade(req.params.encrypted_string)
+		.then(function(grade) {
+			return res.status(200).send(grade);
+		})
+		.catch(function(error) {
+			next(error);
+		})
 	},
 	add(req, res, next) {
-
+		if (!req.body.decrypted_string) {
+			return res.status(400).send({'message': 'Some values are missing'});
+		}	else {
+			const encrypted_string = Auth.hashPassword(req.body.decrypted_string);
+			const grade = [{
+				grade_id: uuidv4(),
+				user_id: req.user.user_id,
+				decrypted_string: req.body.decrypted_string,
+				encrypted_string: encrypted_string,
+				created_date: moment(new Date()),
+				modified_date: moment(new Date())
+			}];
+			queries.addGrade(grade)
+			.then(function() {
+				return res.status(201).send({'message':'Grade successfully added to database'})
+			})
+			.catch(function(error) {
+				next(error);
+			})
+		}
 	},
 	update(req, res, next) {
-		
-	}
+		if (!req.body.decrypted_string) {
+			return res.status(400).send({'message': 'Some values are missing'});
+		}	else {
+			queries.getSingleGrade(req.params.grade_id)
+			.then(function(grade) {
+				if (req.user.user_id === grade.user_id) {
+					const encrypted_string = Auth.hashPassword(req.body.decrypted_string);
+					const updates = {
+						decrypted_string: req.body.decrypted_string,
+						encrypted_string: encrypted_string,
+						modified_date: moment(new Date())
+					};
+					queries.updateGrade(grade.grade_id, updates)
+					.then(function() {
+						return res.status(200).send({'message':'Grade successfully updated'});
+					})
+					.catch(function(error) {
+						next(error);
+					})
+				}	else {
+					return res.status(401).send({'message':'Unauthorized request to update grade'});
+				}
+				
+			})
+			.catch(function(error) {
+				next(error);
+			})
+		}
+	},
 	delete(req, res, next) {
-
+		queries.getSingleGrade(req.params.grade_id)
+		.then(function(grade) {
+			if (req.user.user_id === grade.user_id) {
+				queries.deleteGrade(req.params.grade_id)
+				.then(function() {
+					return res.status(204).send({'message':'Grade successfully deleted'});
+				})
+				.catch(function(error) {
+					next(error);
+				})
+			}	else {
+				return res.status(401).send({'message':'Unauthorized request to delete grade'});
+			}
+		})
+		.catch(function(error) {
+			next(error);
+		})
 	}
 }
+
+module.exports = Grade;
--- routes/gradesRouter.js
+++ routes/gradesRouter.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const Grade = require('../controller/Grade.js');
 const Auth = require('../auth/authentication.js');
 
-router.get('/all', Auth.verifyToken, Grade.getAllGrades);
+router.get('/', Auth.verifyToken, Grade.getAllGrades);
+router.get('/all/:user_id', Auth.verifyToken, Grade.getAllUserGrades);
 router.get('/:encrypted_string', Auth.verifyToken, Grade.getSingleGrade);
 router.post('/', Auth.verifyToken, Grade.add);
-router.update('/:grade_id', Auth.verifyToken, Grade.update);
+router.put('/:grade_id', Auth.verifyToken, Grade.update);
 router.delete('/:grade_id', Auth.verifyToken, Grade.delete);
 
 module.exports = router;
