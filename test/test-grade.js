4554ca1e8dc69b64a61c0dd91ee09d9a4bc19736
Philip Lutz <philiplutz413@gmail.com>
2019-11-06 17:11:14 -0800

    revised tests and separated tests to user and grade files

---
+++ test/test-grade.js
@@ -0,0 +1,388 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../server.js');
+const knex = require('../db/knex.js');
+const queries = require('../db/queries.js');
+const Auth = require('../auth/authentication.js');
+const moment = require('moment');
+const uuidv4 = require('uuid/v4');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Grade Routes', function() { // CREATE tables, seed database, and register a test user
+	beforeEach(function(done) {
+	    knex.migrate.rollback()
+	    .then(function() {
+	    	knex.migrate.latest()
+	    	.then(function() {
+	    		knex.seed.run()
+	    		.then(function() {
+	    			const hashPassword = Auth.hashPassword('12345');
+			    	const testUser = [{
+			    		user_id: uuidv4(),
+						email: 'testUser@gmail.com',
+						password: hashPassword,
+						first_name: 'Test',
+						last_name: 'User',
+						bio: 'Test Bio',
+						private: 'false',
+						admin: 'false',
+						created_date: moment(new Date()),
+						modified_date: moment(new Date())
+			    	}];
+			    	queries.addUser(testUser)
+			    	.then(function() {
+			    		done();
+			    	});
+		    	});
+	    	});
+	    });
+	});
+	afterEach(function(done) { // DROP tables
+	    knex.migrate.rollback()
+	    .then(function() {
+	      done();
+	    });
+	});
+	describe('GET /api/grades', function() {
+		it('should deny request from an unauthenticated user', function(done) {
+			chai.request(app)
+			.get('/api/grades')
+			.end(function(err, res) {
+				res.should.have.status(400);
+				res.body.should.be.a('object');
+				res.body.should.have.property('message');
+				res.body.message.should.equal('Token is not provided');
+				done();
+			});
+		});
+	});
+	describe('GET /api/grades', function() {
+		it('should return all grades to an authenticated user', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				const testToken = Auth.generateToken(user.user_id);
+				chai.request(app)
+				.get('/api/grades/')
+				.set({'x-access-token': testToken})
+				.end(function(err, res) {
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					res.body.length.should.equal(6);
+					res.body[0].should.have.property('grade_id');
+					res.body[0].should.have.property('user_id');
+					res.body[0].should.have.property('decrypted_string');
+					res.body[0].should.have.property('encrypted_string');
+					res.body[0].should.have.property('created_date');
+					res.body[0].should.have.property('modified_date');
+					done();
+				});
+			});
+		});
+	});
+	describe('GET /api/grades/one', function() {
+		it('should deny request from an unauthenticated user', function(done) {
+			chai.request(app)
+			.get('/api/grades/one')
+			.send({
+				encrypted_string: 'this string that will fail'
+			})
+			.end(function(err, res) {
+				res.should.have.status(400);
+				res.body.should.be.a('object');
+				res.body.should.have.property('message');
+				res.body.message.should.equal('Token is not provided');
+				done();
+			});
+		});
+	});
+	describe('GET /api/grades/one', function() {
+		it('should return a specific grade to an authenticated user', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				const testToken = Auth.generateToken(user.user_id);
+				const testId = user.user_id;
+				queries.getAllGrades()
+				.then(function(grades) {
+					const testGrade = grades[0];
+					chai.request(app)
+					.get('/api/grades/one')
+					.set({'x-access-token': testToken})
+					.send({
+						encrypted_string: testGrade.encrypted_string
+					})
+					.end(function(err, res) {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('grade_id');
+						res.body.grade_id.should.be.equal(testGrade.grade_id);
+						res.body.should.have.property('user_id');
+						res.body.user_id.should.equal(testGrade.user_id);
+						res.body.should.have.property('decrypted_string');
+						res.body.decrypted_string.should.equal(testGrade.decrypted_string);
+						res.body.should.have.property('encrypted_string');
+						res.body.encrypted_string.should.equal(testGrade.encrypted_string);
+						done();
+					});
+				});
+			});
+		});
+	});
+	describe('GET /api/grades/all/:user_id', function() {
+		it('should deny request from an unauthenticated user', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				const testUserId = user.user_id;
+				chai.request(app)
+				.get(`/api/grades/all/${testUserId}`)
+				.end(function(err, res) {
+					res.should.have.status(400);
+					res.body.should.be.a('object');
+					res.body.should.have.property('message');
+					res.body.message.should.equal('Token is not provided');
+					done();
+				});
+			});
+		});
+	});
+	describe('GET /api/grades/all/:user_id', function() {
+		it('should return all grades of a specific user to an authenticated user', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				const testToken = Auth.generateToken(user.user_id);
+				const testId = user.user_id;
+				const decrypted_string = "12345";
+				const encrypted_string = Auth.hashPassword(decrypted_string);
+				const testGrade = [{
+					grade_id: uuidv4(),
+					user_id: testId,
+					decrypted_string: decrypted_string,
+					encrypted_string: encrypted_string,
+					created_date: moment(new Date()),
+					modified_date: moment(new Date())
+				}];
+				queries.addGrade(testGrade)
+				.then(function() {
+					chai.request(app)
+					.get(`/api/grades/all/${testId}`)
+					.set({'x-access-token': testToken})
+					.end(function(err, res) {
+						res.should.have.status(200);
+						res.body.should.be.a('array');
+						res.body[0].should.have.property('grade_id');
+						res.body[0].grade_id.should.be.a('string');
+						res.body[0].should.have.property('user_id');
+						res.body[0].user_id.should.equal(testId);
+						res.body[0].should.have.property('decrypted_string');
+						res.body[0].decrypted_string.should.equal('12345')
+						res.body[0].should.have.property('encrypted_string');
+						res.body[0].encrypted_string.should.equal(encrypted_string);
+						done();
+					});
+				});
+			});
+		});
+	});
+	describe('POST /api/grades', function() {
+		it('should deny request from an unauthenticated user', function(done) {
+			const decrypted_string = "12345";
+			const encrypted_string = Auth.hashPassword(decrypted_string);
+			chai.request(app)
+			.post('/api/grades')
+			.send({
+				decrypted_string: decrypted_string,
+				encrypted_string: encrypted_string
+			})
+			.end(function(err, res) {
+				res.should.have.status(400);
+				res.body.should.be.a('object');
+				res.body.should.have.property('message');
+				res.body.message.should.equal('Token is not provided');
+				done();
+			});
+		});
+	});
+	describe('POST /api/grades', function() {
+		it('should create a new grade', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				const testToken = Auth.generateToken(user.user_id);
+				const decrypted_string = "12345";
+				const encrypted_string = Auth.hashPassword(decrypted_string);
+				chai.request(app)
+				.post('/api/grades')
+				.send({
+					decrypted_string: decrypted_string,
+					encrypted_string: encrypted_string
+				})
+				.set({'x-access-token': testToken})
+				.end(function(err, res) {
+					res.should.have.status(201);
+					res.body.should.be.a('object');
+					res.body.should.have.property('message');
+					res.body.message.should.equal('Grade successfully added to database');
+					done();
+				});
+			});
+		});
+	});
+	describe('PUT /api/grades/:grade_id', function() {
+		it('should not allow a user to update a grade of another user', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				let testToken = Auth.generateToken(user.user_id);
+				const testUserId = user.user_id;
+				const decrypted_string = "12345";
+				const encrypted_string = Auth.hashPassword(decrypted_string);
+				const testGrade = [{
+					grade_id: uuidv4(),
+					user_id: testUserId,
+					decrypted_string: decrypted_string,
+					encrypted_string: encrypted_string,
+					created_date: moment(new Date()),
+					modified_date: moment(new Date())
+				}];
+				const gradeId = testGrade[0].grade_id;
+				queries.addGrade(testGrade)
+				.then(function() {
+					queries.getAllUsers()
+					.then(function(users) {
+						for (let i=0; i<users.length; i++) {
+							if (users[i].email !== 'testUser@gmail.com') {
+								testToken = Auth.generateToken(users[i].user_id);
+								break;
+							}
+						}
+						chai.request(app)
+						.put(`/api/grades/${gradeId}`)
+						.set({'x-access-token': testToken})
+						.send({
+							decrypted_string: "54321"
+						})
+						.end(function(err, res) {
+							res.should.have.status(401);
+							res.body.should.be.a('object');
+							res.body.should.have.property('message');
+							res.body.message.should.equal('Unauthorized request to update grade');
+							done();
+						});
+					});
+				});
+			});
+		});
+	});
+	describe('PUT /api/grades/:grade_id', function() {
+		it('should update a grade of the user making the request', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				const testToken = Auth.generateToken(user.user_id);
+				const testId = user.user_id;
+				const decrypted_string = "12345";
+				const encrypted_string = Auth.hashPassword(decrypted_string);
+				const testGrade = [{
+					grade_id: uuidv4(),
+					user_id: testId,
+					decrypted_string: decrypted_string,
+					encrypted_string: encrypted_string,
+					created_date: moment(new Date()),
+					modified_date: moment(new Date())
+				}];
+				const gradeId = testGrade[0].grade_id;
+				queries.addGrade(testGrade)
+				.then(function() {
+					chai.request(app)
+					.put(`/api/grades/${gradeId}`)
+					.set({'x-access-token': testToken})
+					.send({
+						decrypted_string: "54321"
+					})
+					.end(function(err, res) {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('message');
+						res.body.message.should.equal('Grade successfully updated');
+						done();
+					});
+				});
+			});
+		});
+	});
+	describe('DELETE /api/grades/:grade_id', function() {
+		it('should not allow a user to delete the grade of another user', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				let testToken = Auth.generateToken(user.user_id);
+				const testUserId = user.user_id;
+				const decrypted_string = "12345";
+				const encrypted_string = Auth.hashPassword(decrypted_string);
+				const testGrade = [{
+					grade_id: uuidv4(),
+					user_id: testUserId,
+					decrypted_string: decrypted_string,
+					encrypted_string: encrypted_string,
+					created_date: moment(new Date()),
+					modified_date: moment(new Date())
+				}];
+				const gradeId = testGrade[0].grade_id;
+				queries.addGrade(testGrade)
+				.then(function() {
+					queries.getAllUsers()
+					.then(function(users) {
+						for (let i=0; i<users.length; i++) {
+							if (users[i].email !== 'testUser@gmail.com') {
+								testToken = Auth.generateToken(users[i].user_id);
+								break;
+							}
+						}
+						chai.request(app)
+						.delete(`/api/grades/${gradeId}`)
+						.set({'x-access-token': testToken})
+						.end(function(err, res) {
+							res.should.have.status(401);
+							res.body.should.be.a('object');
+							res.body.should.have.property('message');
+							res.body.message.should.equal('Unauthorized request to delete grade');
+							done();
+						});
+					});
+				});
+			});
+		});
+	});
+	describe('DELETE /api/grades/:grade_id', function() {
+		it('should delete a grade of the user making the request', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				const testToken = Auth.generateToken(user.user_id);
+				const testId = user.user_id;
+				const decrypted_string = "12345";
+				const encrypted_string = Auth.hashPassword(decrypted_string);
+				const testGrade = [{
+					grade_id: uuidv4(),
+					user_id: testId,
+					decrypted_string: decrypted_string,
+					encrypted_string: encrypted_string,
+					created_date: moment(new Date()),
+					modified_date: moment(new Date())
+				}];
+				const gradeId = testGrade[0].grade_id;
+				queries.addGrade(testGrade)
+				.then(function() {
+					chai.request(app)
+					.delete(`/api/grades/${gradeId}`)
+					.set({'x-access-token': testToken})
+					.end(function(err, res) {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('message');
+						res.body.message.should.equal('Grade successfully deleted');
+						done();
+					});
+				});
+			});
+		});
+	});
+});
---
+++ test/test-user.js
@@ -0,0 +1,282 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../server.js');
+const knex = require('../db/knex.js');
+const queries = require('../db/queries.js');
+const Auth = require('../auth/authentication.js');
+const moment = require('moment');
+const uuidv4 = require('uuid/v4');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('User Routes', function() {
+	beforeEach(function(done) { // CREATE tables, seed database, and register a test user
+	    knex.migrate.rollback()
+	    .then(function() {
+	    	knex.migrate.latest()
+	    	.then(function() {
+	    		knex.seed.run()
+	    		.then(function() {
+	    			const hashPassword = Auth.hashPassword('12345');
+			    	const testUser = [{
+			    		user_id: uuidv4(),
+						email: 'testUser@gmail.com',
+						password: hashPassword,
+						first_name: 'Test',
+						last_name: 'User',
+						bio: 'Test Bio',
+						private: 'false',
+						admin: 'false',
+						created_date: moment(new Date()),
+						modified_date: moment(new Date())
+			    	}];
+			    	queries.addUser(testUser)
+			    	.then(function() {
+			    		done();
+			    	});
+		    	});
+	    	});
+	    });
+	});
+	afterEach(function(done) { // DROP tables
+	    knex.migrate.rollback()
+	    .then(function() {
+	      done();
+	    });
+	});
+	describe('POST /api/users/register', function() {
+		it('should prevent a user from signing up with an existing email address', function(done) {
+			chai.request(app)
+			.post('/api/users/register')
+			.send({
+				email: 'testUser@gmail.com',
+				password: '1234512345',
+				first_name: 'Phil',
+				last_name: 'Lutz',
+				bio: 'This is my bio',
+				private: 'false'
+			})
+			.end(function(err, res) {
+				res.should.have.status(400);
+				res.body.should.be.a('object');
+				res.body.should.have.property('message');
+				res.body.message.should.equal('Please enter a different email address');
+				done();
+			});
+		});
+	});
+	describe('POST /api/users/register', function() {
+		it('should register a new user', function(done) {
+			chai.request(app)
+			.post('/api/users/register')
+			.send({
+				email: 'philiplutz413@gmail.com',
+				password: '1234512345',
+				first_name: 'Phil',
+				last_name: 'Lutz',
+				bio: 'This is my bio',
+				private: 'false'
+			})
+			.end(function(err, res) {
+				res.should.have.status(201);
+				res.body.should.be.a('object');
+				res.body.should.have.property('message');
+				res.body.message.should.equal('User successfully registered');
+				done();
+			});
+		});
+	});
+	describe('POST /api/users/login', function() {
+		it('should authenticate a user using an incorrect password', function(done) {
+			chai.request(app)
+			.post('/api/users/login')
+			.send({
+				email: 'testUser@gmail.com',
+				password: '123'
+			})
+			.end(function(err, res) {
+				res.should.have.status(400);
+				res.body.should.be.a('object');
+				res.body.should.have.property('message');
+				res.body.message.should.equal('The credentials you provided are incorrect');
+				done();
+			});
+		});
+	});
+	describe('POST /api/users/login', function() {
+		it('should login a user', function(done) {
+			chai.request(app)
+			.post('/api/users/login')
+			.send({
+				email: 'testUser@gmail.com',
+				password: '12345'
+			})
+			.end(function(err, res) {
+				res.should.have.status(200);
+				res.body.should.be.a('object');
+				res.body.should.have.property('token');
+				res.body.token.should.be.a('string');
+				done();
+			});
+		});
+	});
+	describe('GET /api/users/:user_id', function() {
+		it('should deny request from an unauthenticated user', function(done) {
+			queries.getUserEmail('testUser@gmail.com')
+			.then(function(user) {
+				const testUserId = user.user_id;
+				chai.request(app)
+				.get(`/api/users/${testUserId}`)
+				.end(function(err, res) {
+					res.should.have.status(400);
+					res.body.should.be.a('object');
+					res.body.should.have.property('message');
+					res.body.message.should.equal('Token is not provided');
+					done();
+				});
+			});
+		});
+	});
+	describe('GET /api/users/:user_id', function() {
+		it('should return public information of a specific user to an authenticated user', function(done) {
+			queries.getAllUsers()
+			.then(function(users) {
+				let testUserId = null;
+				let anotherUserId = null;
+				let testToken = null;
+				for (let i=0; i<users.length; i++) {
+					if (users[i].email === 'testUser@gmail.com') {
+						testUserId = users[i].user_id;
+					}	else {
+						anotherUserId = users[i].user_id;
+						testToken = Auth.generateToken(anotherUserId);
+					}
+					if (anotherUserId !== null && testToken !== null && testUserId !== null) {
+						break;
+					}
+				}
+				chai.request(app)
+				.get(`/api/users/${testUserId}`)
+				.set({'x-access-token': testToken})
+				.end(function(err, res) {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('user_id');
+					res.body.user_id.should.equal(testUserId);
+					res.body.should.have.property('first_name');
+					res.body.first_name.should.equal('Test');
+					res.body.should.have.property('last_name');
+					res.body.last_name.should.equal('User');
+					res.body.should.have.property('bio');
+					res.body.bio.should.equal('Test Bio');
+					done();
+				});
+			});
+		});
+	});
+	describe('PUT /api/users', function() {
+		it('should deny request from an unauthenticated user', function(done) {
+			chai.request(app)
+			.put('/api/users')
+			.send({
+				bio: 'There is no user associated with this request so it should fail'
+			})
+			.end(function(err, res) {
+				res.should.have.status(400);
+				res.body.should.be.a('object');
+				res.body.should.have.property('message');
+				res.body.message.should.equal('Token is not provided');
+				done();
+			});
+		});
+	});
+	describe('PUT /api/users', function() {
+		it('should update the information of the user making the request', function(done) {
+			queries.getAllUsers()
+			.then(function(users) {
+				const testUserId = users[0].user_id;
+				const testToken = Auth.generateToken(testUserId);
+				chai.request(app)
+				.put('/api/users')
+				.set({'x-access-token': testToken})
+				.send({
+					bio: 'I modified my bio to have more words'
+				})
+				.end(function(err, res) {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('message');
+					res.body.message.should.equal('User information successfully updated');
+					done();
+				});
+			});
+		});
+	});
+	describe('DELETE /api/users/:user_id', function() {
+		it('should prevent user deletion without being an admin', function(done) {
+			queries.getAllUsers()
+			.then(function(users) {
+				let testToken = null;
+				let anotherUserId = null;
+				for (let i=0; i<users.length; i++) {
+					if (users[i].email === 'testUser@gmail.com') {
+						testToken = Auth.generateToken(users[i].user_id);
+					}	else {
+						anotherUserId = users[i].user_id;
+					}
+					if (testToken !== null && anotherUserId !== null) {
+						break;
+					}
+				}
+				chai.request(app)
+				.delete(`/api/users/${anotherUserId}`)
+				.set({'x-access-token': testToken})
+				.end(function(err, res) {
+					res.should.have.status(401);
+					res.body.should.be.a('object');
+					res.body.should.have.property('message');
+					res.body.message.should.equal('You do not have access to delete users');
+					done();
+				});
+			});
+		});
+	});
+	describe('DELETE /api/users/:user_id', function() {
+		it('should only allow an admin to delete a user', function(done) {
+			const hashPassword = Auth.hashPassword('12345');
+		    const testAdminUser = [{
+		   		user_id: uuidv4(),
+				email: 'testAdminUser@gmail.com',
+				password: hashPassword,
+				first_name: 'Test',
+				last_name: 'Admin',
+				bio: 'Test Admin Bio',
+				private: 'false',
+				admin: 'true',
+				created_date: moment(new Date()),
+				modified_date: moment(new Date())
+		    }];
+		    const testToken = Auth.generateToken(testAdminUser[0].user_id);
+		    queries.addUser(testAdminUser)
+		   	.then(function() {
+		   		queries.getUserEmail('testUser@gmail.com')
+		   		.then(function(user) {
+		   			const testUserId = user.user_id;
+		   			chai.request(app)
+		   			.delete(`/api/users/${testUserId}`)
+		   			.set({'x-access-token': testToken})
+		  			.end(function(err, res) {
+	 					res.should.have.status(200);
+		   				res.body.should.be.a('object');
+						res.body.should.have.property('message');
+						res.body.message.should.equal('User successfully deleted');
+						done();
+		   			});
+		   		});
+		   	});
+		});
+	});
+});
